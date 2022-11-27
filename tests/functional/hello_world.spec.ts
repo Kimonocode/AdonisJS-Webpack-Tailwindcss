1f12c0bc47172581298afc3840b3e09193fc2f72
--get <ronald.github.dev@gmail.com>
2022-11-27 21:47:25 +0100

    refact

--- tests/functional/hello_world.spec.ts
+++ tests/functional/hello_world.spec.ts
@@ -2,7 +2,6 @@ import { test } from '@japa/runner'
 
 test('display welcome page', async ({ client }) => {
   const response = await client.get('/')
-
   response.assertStatus(200)
-  response.assertTextIncludes('<h1 class="title"> It Works! </h1>')
+  response.assertTextIncludes('<h1 class="title">Welcome to Adonisjs whit Webpack and Tailwindcss !</h1>')
 })
